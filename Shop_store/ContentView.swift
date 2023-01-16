5cc6a80efa599040f902287d81c56714024442f1
kunyuli12 <kunyuli16@gmail.com>
2023-01-16 12:51:08 +0800

    Initial Commit

---
+++ Shop_store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shop_store
+//
+//  Created by 暨大附中03 on 2023/1/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shop_store/Shop_storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shop_storeApp.swift
+//  Shop_store
+//
+//  Created by 暨大附中03 on 2023/1/16.
+//
+
+import SwiftUI
+
+@main
+struct Shop_storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
